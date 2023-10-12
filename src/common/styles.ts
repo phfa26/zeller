9c5072de1d8405a30fd8c742e1202e687fe136f9
phfa26 <phfa@outlook.com.au>
2023-10-12 15:21:56 +1100

    Added new styled component <hr/> as list divider with margins above and below

---
+++ src/common/styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Divider } from '@mui/material';
+
+export const ListDivider = styled(Divider)`
+  color: #f5f5f5;
+  margin: 20px 0;
+  width: 100%;
+`;
