a4edd38a5da1cc2f0564bfeae9329fccc9fd4fd1
phfa26 <phfa@outlook.com.au>
2023-10-12 15:18:08 +1100

    added commom styles

--- src/components/UserTypeSelector/styles.ts
+++ src/components/UserTypeSelector/styles.ts
@@ -1,7 +1,25 @@
+// styles.ts or styles.js
 import { styled } from '@mui/system';
-import { RadioGroup } from '@mui/material';
+import { RadioGroup, Radio, FormControlLabel, FormControl } from '@mui/material';
+
+export const StyledFormControl = styled(FormControl)`
+  margin-top: 20px;
+`;
 
 export const StyledRadioGroup = styled(RadioGroup)`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
-  margin-top: 20px;
+`;
+
+export const StyledRadio = styled(Radio)`
+  color: #E1F5FD !important;
+  &.Mui-checked {
+    color: #00579A !important;
+  }
+`;
+
+export const StyledFormControlLabel = styled(FormControlLabel)<{ selected?: boolean }>`
+  background-color: ${({ selected }) => (selected ? '#E1F5FD' : 'transparent')};
+  border-radius: 8px;
 `;
