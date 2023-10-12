f16e27d874f281bf1f627661b0367bcac8074295
phfa26 <phfa@outlook.com.au>
2023-10-12 16:07:53 +1100

    added basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { fetchUsers } from './utils/api';
+import { UserType } from './types';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./utils/api', () => ({
+  fetchUsers: jest.fn(),
+}));
+
+describe('App component', () => {
+  test('renders user types title', async () => {
+    render(<App />);
+    const titleElement = await screen.findByText(/user types/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test('fetches users and displays them', async () => {
+    const users = [
+      { id: '1', name: 'Test User', role: UserType.ADMIN },
+    ];
+
+    (fetchUsers as jest.Mock).mockResolvedValue(users);
+
+    render(<App />);
+
+    const userNameElement = await screen.findByText(/test user/i);
+    const userRoleElement = await screen.findAllByText(/admin/i);
+
+    expect(userNameElement).toBeInTheDocument();
+    expect(userRoleElement.length).toBeGreaterThan(1); // Makes sure that when a user is an admin, it's also being displayed on the screen, other than the 'Admin' from the select.
+  });
+
 });
