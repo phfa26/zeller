452665e995814182623af7c7ec8d506f0ecea9a3
phfa26 <phfa@outlook.com.au>
2023-10-12 12:56:13 +1100

    Used AMplify for queries and MUI theming

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';  // path to your aws-exports.js file
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports.js';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import theme from './theme';
 
-Amplify.configure(awsconfig);
+(Amplify as any).configure(awsconfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
