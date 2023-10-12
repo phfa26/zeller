e3a44fbbcc0eb015705b7af9f3d0fe7f5988569d
phfa26 <phfa@outlook.com.au>
2023-10-12 15:21:20 +1100

    User type selector better typing definition for user type actions and added custom styles to existing components

--- src/components/UserTypeSelector/UserTypeSelector.tsx
+++ src/components/UserTypeSelector/UserTypeSelector.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { UserType } from '../../types';
-import { StyledRadioGroup } from './styles';
-import { FormControlLabel, Radio, FormControl, FormLabel } from '@mui/material';
+import { StyledRadioGroup, StyledRadio, StyledFormControlLabel, StyledFormControl } from './styles';
+import { ListDivider } from '../../common/styles';
 
-interface UserTypeSelectorProps {
+interface UserTypeControlProps {
   userType: UserType;
-  setUserType: (value: UserType) => void;
+  setUserType: React.Dispatch<React.SetStateAction<UserType>>;
 }
 
-const UserTypeSelector: React.FC<UserTypeSelectorProps> = ({ userType, setUserType }) => {
+const UserTypeControl: React.FC<UserTypeControlProps> = ({ userType, setUserType }) => {
   return (
-    <FormControl component="fieldset">
-      <FormLabel component="legend">User Type</FormLabel>
-      <StyledRadioGroup row value={userType} onChange={(e) => setUserType(e.target.value as UserType)}>
-        <FormControlLabel value="ADMIN" control={<Radio />} label="Admin" />
-        <FormControlLabel value="MANAGER" control={<Radio />} label="Manager" />
-      </StyledRadioGroup>
-    </FormControl>
-  );
+    <>
+      <StyledFormControl>
+        <StyledRadioGroup row value={userType} onChange={(e: { target: { value: string; }; }) => setUserType(e.target.value as UserType)}>
+          <StyledFormControlLabel value="ADMIN" control={<StyledRadio />} label="Admin" selected={userType === 'ADMIN'}/>
+          <StyledFormControlLabel value="MANAGER" control={<StyledRadio />} label="Manager" selected={userType === 'MANAGER'}/>
+        </StyledRadioGroup>
+      </StyledFormControl>
+      <ListDivider />
+    </>
+  ); 
 };
 
-export default UserTypeSelector;
+export default UserTypeControl;
