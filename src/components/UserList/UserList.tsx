5ce29a853dafa21b0a3ec77ae21d1400176aafd6
phfa26 <phfa@outlook.com.au>
2023-10-12 15:19:28 +1100

    Adde title to User list based on users role and updated styles

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { User } from '../../types';
+import { User, UserType } from '../../types';
 import UserProfile from '../UserProfile/UserProfile';
-import { List, ListItem } from '@mui/material';
+import { List } from '@mui/material';
+import { StyledListItem, Title } from './styles';
+import { ListDivider } from '../../common/styles';
 
 interface UserListProps {
   users: User[];
+  role: UserType;
 }
 
-const UserList: React.FC<UserListProps> = ({ users }) => {
+const UserList: React.FC<UserListProps> = ({ users, role }) => {
+  const getUserRoleTitle = (role: UserType) => {
+    switch(role.toLowerCase()) {
+      case 'admin':
+        return 'Admin Users';
+      case 'manager':
+        return 'Manager Users';
+      default:
+        return 'Users';
+    }
+  };
+
   return (
-    <List>
-      {users.map((user) => (
-        <ListItem key={user.id}>
-          <UserProfile name={user.name} />
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Title variant="h4">{getUserRoleTitle(role)}</Title> {/* Display the title here */}
+      <List>
+        {users.map((user) => (
+          <StyledListItem key={user.id}>
+            <UserProfile user={user} />
+          </StyledListItem>
+        ))}
+      </List>
+      <ListDivider />
+    </>
   );
 };
 
---
+++ src/components/UserList/styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { Title as AppTitle } from '../../App.styles';
+import { ListItem } from '@mui/material';
+
+export const Title = styled(AppTitle)`
+  marginBottom: '20px';
+`
+export const StyledListItem = styled(ListItem)`
+  padding-left: 0;
+`
